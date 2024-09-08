56b32f10f0d67f3262e440aa0811f3537d7d5dc6
dz6(1) <vladyuzevuts@gmail.com>
2024-09-08 20:33:22 +0300

    Check dz

---
+++ Dz16.py
@@ -0,0 +1,23 @@
+class Math:
+    @staticmethod
+    def plus(num1,num2):
+        return num1 + num2
+
+    def minus(num1, num2):
+        return num1 - num2
+
+    def multiply(num1, num2):
+        return num1 * num2
+
+    def divide(num1, num2):
+        return num1 / num2
+
+num1=int(input("Enter first number:\t"))
+num2=int(input("Enter sec number:\t"))
+print(Math.plus(num1,num2))
+print(Math.minus(num1,num2))
+print(Math.multiply(num1,num2))
+print(Math.divide(num1,num2))
+
+
+
